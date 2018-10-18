c27810cacdfbcc6835384e904acca03bff736db6
GoshanX <goshanx3@gmail.com>
2018-10-18 20:51:51 +0300

    Initial commit

---
+++ Stack_20.09/CodeFile1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Stack {
+    char[] stck; // массив, содержащий стек
+    int tos;    // индекс вершины стека
+
+        public Stack(int size) {
+            stck = new char[size];
+            tos = 0;
+        }
+    public void Push(char ch) {
+        if(tos == stck.Length){
+            Console.WriteLine("  - Стек заполнен.");
+            return;
+        }
+        stck[tos] = ch;
+        tos++;
+    }
+    public char Pop() {
+        if(tos == 0) {
+            Console.WriteLine(" - Стек пуст.");
+            return (char)0;
+        }
+        tos--;
+        return stck[tos];
+    }
+    public bool IsFull() {
+        return tos == stck.Length;
+    }
+    public bool IsEmpty() {
+        return tos == 0;
+    }
+    public int Capacity() {
+        return stck.Length;
+    }
+    public int GetNum() {
+        return tos;
+    }
+    
+}
+
+
+
---
+++ Stack_20.09/TestStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+    class TestStack
+    {
+        static void Main()
+        {
+            Stack stk1 = new Stack(10);
+            Stack stk2 = new Stack(10);
+            Stack stk3 = new Stack(10);
+            char ch;
+            int i;
+
+            Console.WriteLine("Поместить символы A-j  в стек stk1.");
+            for (i = 0; !stk1.IsFull(); i++)
+                stk1.Push((char)('A' + i));
+
+            if(stk1.IsFull()) Console.WriteLine( " Стек stk1 заполнен");
+
+            Console.WriteLine(" Содержимое стека stk1: ");
+            while (!stk1.IsEmpty())
+            {
+                ch = stk1.Pop();
+                Console.Write(ch);
+            }
+
+            Console.WriteLine();
+
+            if(stk1.IsEmpty() ) Console.WriteLine(" Стек stk1 пуст.");
+
+            Console.WriteLine("Вновь посместить символы A-J в стек stk1.");
+            for(i = 0;!stk1.IsFull(); i++)
+                stk1.Push((char)('A'+ i));
+
+            Console.WriteLine("А теперь извлечь символы из стека stk1\n" + "и поместить их в стек stk2");
+            while (!stk1.IsEmpty())
+            {
+                ch = stk1.Pop();
+                stk2.Push(ch);
+            }
+            Console.WriteLine("Содержимое стека stk2: ");
+            while (!stk2.IsEmpty())
+            {
+                Console.Write(stk2.Pop());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Поместить 5 символов в стек stk3");
+            for (i = 0; i < 5; i++)
+            {
+                stk3.Push((char)('A' + i));
+            }
+            Console.WriteLine("Емкость стека stk3: " + stk3.Capacity());
+            Console.WriteLine("Количество объектов в стеке stk3: " + stk3.GetNum());
+
+        }
+    }
+
